f29714e95a8f1223975511c506219eac14586f25
Madhu5A3 <madhuv299@gmail.com>
2020-06-12 23:27:23 +0530

    Update: Git Org LifeCycle Automating Shell Scripts

---
+++ git-repo-cacp/f-c-d-u-c-p-p.sh
@@ -0,0 +1,2 @@
+### Shell Script to Automate Fork(Remote Upstream Repo) - Clone(Local) - Development(Local) - Upstream(Checking In Latest Changes From Upstream) - Commit - Push (Origin) - Pull(Rebase)
+echo ('Under a shell of plenty of things')
